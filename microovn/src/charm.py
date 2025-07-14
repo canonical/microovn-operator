9aa3a0f5e3c679d11c33882c9ed7b86aac76d4bf
MJ Ponsonby <mj.ponsonby@canonical.com>
2025-07-14 15:56:22 +0100

    Allow the charm to work in dataplane mode
    provided the ovsdb-external relation exists put the charm in dataplane mode
    by disabling central services and directing towards the addresses
    provided by ovsdb-external
    
    Signed-off-by: MJ Ponsonby <mj.ponsonby@canonical.com>

--- microovn/src/charm.py
+++ microovn/src/charm.py
@@ -8,6 +8,7 @@ import ops
 
 from charms.microovn.v0.ovsdb import OVSDBProvides
 from charms.microcluster_token_distributor.v0.token_distributor import TokenConsumer
+from charms.ovn_central_k8s.v0.ovsdb import OVSDBCMSRequires
 from typing import Optional
 
 from charms.tls_certificates_interface.v4.tls_certificates import (
@@ -22,6 +23,7 @@ logger = logging.getLogger(__name__)
 OVSDB_RELATION = "ovsdb"
 WORKER_RELATION = "cluster"
 CERTIFICATES_RELATION = "certificates"
+OVSDBCMD_RELATION = "ovsdb-external"
 
 CSR_ATTRIBUTES = CertificateRequestAttributes(
     common_name="Charmed MicroOVN",
@@ -46,6 +48,7 @@ class MicroovnCharm(ops.CharmBase):
     def __init__(self, framework: ops.Framework):
         super().__init__(framework)
         self._stored.set_default(in_cluster=False)
+        self._stored.set_default(dataplane=False)
 
         self.certificates = TLSCertificatesRequiresV4(
             charm=self,
@@ -65,12 +68,52 @@ class MicroovnCharm(ops.CharmBase):
             command_name=["microovn", "cluster"]
         )
 
+        self.ovsdbcms_requires = OVSDBCMSRequires(
+            charm=self,
+            relation_name=OVSDBCMD_RELATION,
+        )
+
         framework.observe(self.on.install, self._on_install)
         framework.observe(self.on[WORKER_RELATION].relation_changed,
                             self._on_cluster_changed)
+
         framework.observe(
             self.certificates.on.certificate_available, self._on_certificates_available
         )
+        framework.observe(
+            self.ovsdbcms_requires.on.ready, self._on_ovsdbcms_ready
+        )
+
+    def _set_central_ips_config(self):
+        addresses =  self.ovsdbcms_requires.bound_addresses()
+        err, _ = call_microovn_command("config","set","ovn.central-ips",
+                                        ",".join(addresses))
+        if err:
+            logger.error("calling config set failed with code {0}".format(err))
+            return False
+        return True
+
+
+    def _dataplane_mode(self):
+        if not self._stored.in_cluster or self._stored.dataplane or \
+           not self.model.get_relation(OVSDBCMD_RELATION) or \
+           not self.ovsdbcms_requires.remote_ready():
+            return
+
+        self.unit.status = ops.MaintenanceStatus("switching to dataplane mode")
+        call_microovn_command("disable", "central",
+                              "--allow-disable-last-central")
+        if self.unit.is_leader():
+            if not self._set_central_ips_config():
+                self.unit.status = ops.MaintenanceStatus(
+                    "failed switching to dataplane mode")
+                return False
+        self._stored.dataplane=True
+        self.unit.status = ops.ActiveStatus()
+
+
+    def _on_ovsdbcms_ready(self, _: ops.EventBase):
+        self._dataplane_mode()
 
     def _on_certificates_available(self, _: ops.EventBase):
         """Check if the certificate or private key needs an update and perform the update.
@@ -136,6 +179,7 @@ class MicroovnCharm(ops.CharmBase):
     def _on_cluster_changed(self, event: ops.RelationChangedEvent):
         if self._stored.in_cluster:
             self.ovsdb_provides.update_relation_data()
+            self._dataplane_mode()
 
 if __name__ == "__main__":  # pragma: nocover
     ops.main(MicroovnCharm)
